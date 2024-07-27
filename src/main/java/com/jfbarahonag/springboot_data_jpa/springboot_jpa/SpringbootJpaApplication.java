a0b04634c87a598b5b7e83d46b2462d9f093f005
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-27 11:59:30 -0500

    Main class has been changed to use command line instead of web

--- src/main/java/com/jfbarahonag/springboot_data_jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/jfbarahonag/springboot_data_jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,13 +1,18 @@
 package com.jfbarahonag.springboot_data_jpa.springboot_jpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootJpaApplication {
+public class SpringbootJpaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 	}
 
 }
