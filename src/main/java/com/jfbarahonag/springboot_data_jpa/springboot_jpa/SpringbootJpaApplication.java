d922f9da56dec350136cc758c92d5c33277e2071
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-27 11:58:11 -0500

    first commit

---
+++ src/main/java/com/jfbarahonag/springboot_data_jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jfbarahonag.springboot_data_jpa.springboot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfbarahonag/springboot_data_jpa/springboot_jpa/SpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfbarahonag.springboot_data_jpa.springboot_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
