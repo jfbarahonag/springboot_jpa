22dfd565338979cd9d9bb039b16b934599783aed
jfbarahonag <jfbarahonag@unal.edu.co>
2024-07-28 11:28:40 -0500

    adding a seed file

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO persons (firstname, lastname, programming_language) VALUES ('Juan', 'Perez', 'Java');
+INSERT INTO persons (firstname, lastname, programming_language) VALUES ('Maria', 'Garcia', 'Python');
+INSERT INTO persons (firstname, lastname, programming_language) VALUES ('Carlos', 'Rodriguez', 'JavaScript');
+INSERT INTO persons (firstname, lastname, programming_language) VALUES ('Ana', 'Martinez', 'C#');
+INSERT INTO persons (firstname, lastname, programming_language) VALUES ('Luis', 'Hernandez', 'Ruby');
